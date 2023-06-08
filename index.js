348fd1a68311528a084d606af48a3a45ad8571ce
saad <saadansari74002@gmail.com>
2023-06-08 11:43:03 +0530

    add some more thing and finish the project git commit -m 'finish the project'

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // let person = prompt('❌ You cannot withdraw more than what you have!');
-
-class Bank {
-  constructor(balance) {
+class Bank{
+  constructor(balance){
     this.balance = balance;
   }
 
-  withdrew(amount) {
-    if (this.balance - amount <= 0) {
+  withdrew(amount){
+    if (this.balance - amount <= 10){
       messageDiv.innerText = `('❌ You cannot withdraw less 100 than what you have!')`;
-      console.log('❌ You cannot withdraw more than what you have!');
-      console.log({ balance: this.balance });
+      console.log('your amount is less then this amount');
+      console.log({balance:this.balance});
       return;
     }
     this.balance -= amount;
-    console.log('withdrew', `$${amount}`);
-    console.log({ balance: this.balance });
+    console.log('withdrew',`$${amount}`);
+    console.log({balance:this.balance});
   }
-
-  deposit(amount) {
+  
+  deposit(amount){
+    // if (this.balance + amount = amount){
     this.balance += amount;
     console.log('deposit', `${amount}`);
-    console.log({ balance: this.balance });
+    console.log({balance:this.balance});
+    // return
   }
 }
 
-const saadChecking = new Bank(0);
-console.log(saadChecking.balance);
-
+const asadChecking = new Bank(0);
+console.log(asadChecking.balance);
 
 const amountInput = document.getElementById('amount');
 const depositButton = document.getElementById('deposit-btn');
@@ -35,14 +35,55 @@ const balanceDiv = document.getElementById('total-amt');
 const messageDiv = document.getElementById('message-amt')
 
 withdrewButton.onclick = () => {
-  const amount = Number(amountInput.value);
-  saadChecking.withdrew(amount);
-  balanceDiv.innerHTML = `Total amount $${saadChecking.balance}`;
+  const amount = Number (amountInput.value);
+  asadChecking.withdrew(amount);
+  balanceDiv.innerHTML = `Total amount $${asadChecking.balance}`;
 };
 
 depositButton.onclick = () => {
-  const amount = Number(amountInput.value);
-  saadChecking.deposit(amount);
-  balanceDiv.innerHTML = `Total amount $${saadChecking.balance}`;
+  const amount = Number (amountInput.value);
+  asadChecking.deposit(amount);
+  balanceDiv.innerHTML = `Total amount $${asadChecking.balance}`;
 };
+
+// class Bank {
+//   constructor(balance) {
+//     this.balance = balance;
+//   }
+
+//   withdrew(amount) {
+//     if (this.balance - amount <= 100) {
+//       messageDiv.innerText = `('❌ You cannot withdraw less 100 than what you have!')`;
+//       console.log('❌ You cannot withdraw more than what you have!');
+//       console.log({ balance: this.balance });
+//       return;
+//     }
+//     this.balance -= amount;
+//     console.log('withdrew', `$${amount}`);
+//     console.log({ balance: this.balance });
+//   }
+
+//   deposit(amount) {
+//     this.balance += amount;
+//     console.log('deposit', `${amount}`);
+//     console.log({ balance: this.balance });
+//   }
+// }
+
+// const saadChecking = new Bank(0);
+// console.log(saadChecking.balance);
+
+
+
+// withdrewButton.onclick = () => {
+//   const amount = Number(amountInput.value);
+//   saadChecking.withdrew(amount);
+//   balanceDiv.innerHTML = `Total amount $${saadChecking.balance}`;
+// };
+
+// depositButton.onclick = () => {
+//   const amount = Number(amountInput.value);
+//   saadChecking.deposit(amount);
+//   balanceDiv.innerHTML = `Total amount $${saadChecking.balance}`;
+// };
 
